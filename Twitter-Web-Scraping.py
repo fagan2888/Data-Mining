581a56174c9f0cff626ccec0c3868406449d321a
Matt Wilchek <mwilchek@gmu.edu>
2017-11-29 19:35:14 -0500

    edited for cleaner results

--- Twitter-Web-Scraping.py
+++ Twitter-Web-Scraping.py
@@ -1,6 +1,7 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import json
 
 #consumer key, consumer secret, access token, access secret.
 ckey=" "
@@ -10,8 +11,13 @@ asecret=" "
 
 class listener(StreamListener):
     def on_data(self, data):
-        print('\n',data)
-        return(True)
+        all_data = json.loads(data)
+        user = all_data["user"]
+        screen_name = user["screen_name"]
+        tweet = all_data["text"]
+        print(screen_name, ' -> ', tweet)
+        return True
+    
     def on_error(self, status):
         print('Wait a few minutes and try again, Error Code:', status)
         return(False)
